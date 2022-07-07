c5ba6d9355e44a4da1a48a9cafe6f2e192c84b97
arthur miranda <miranda_chaves@hotmail.com>
2022-07-07 17:17:13 -0300

    first exercise

--- 01-React/01-Tooling/02-Manipulating-Strings/lib/01_dash_insertion.js
+++ 01-React/01-Tooling/02-Manipulating-Strings/lib/01_dash_insertion.js
@@ -1,6 +1,16 @@
-function insertDash(word) {
-  // TODO: implement the method and return word with dashes
-  return null;
+function isConsonant(letter) {
+  return /[bcdfghjklmnpqrstvwxyz]/i.test(letter);
 }
 
+function insertDash(word) {
+  if (!word || word.length === 0) {
+    return "";
+  }
+  return word.split("").map((letter, i, letters) => {
+    if (i > 0 && isConsonant(letters[i - 1]) && isConsonant(letter)) {
+      return `-${letter}`;
+    }
+    return letter;
+  }).join("");
+}
 module.exports = insertDash;
--- 01-React/01-Tooling/02-Manipulating-Strings/lib/02_counting_words.js
+++ 01-React/01-Tooling/02-Manipulating-Strings/lib/02_counting_words.js
@@ -1,5 +1,9 @@
 function occurrences(text, word) {
-  return null;
+  if (!text || text.length === 0) {
+    return 0;
+  }
+  const reducer = (count, term) => count + (term.toLowerCase() === word.toLowerCase() ? 1 : 0);
+  return text.split(" ").reduce(reducer, 0);
 }
 
 module.exports = occurrences;

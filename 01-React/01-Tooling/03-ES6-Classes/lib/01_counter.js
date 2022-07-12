96e068d3ea5bf4b309eee22b2edcc8fd5be129a9
arthur miranda <miranda_chaves@hotmail.com>
2022-07-11 22:42:53 -0300

    done

--- 01-React/01-Tooling/03-ES6-Classes/lib/01_counter.js
+++ 01-React/01-Tooling/03-ES6-Classes/lib/01_counter.js
@@ -1,10 +1,16 @@
 class Counter {
   constructor(text) {
-    // TODO: build an internal Map of word => occurrences.
+    this.map = new Map();
+    if (text && text.length > 0) {
+      this.split(" ").forEach((term) => {
+        const lowerTerm = term.toLowerCase();
+        this.map.set(lowerTerm, (this.map.get(lowerTerm) || 0) + 1);
+      });
+    }
   }
 
   occurrences(word) {
-    // TODO: return the number of occurrences
+    return this.map.get(word.toLowerCase()) || 0;
   }
 }
 
